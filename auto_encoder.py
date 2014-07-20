b164e2e053ae1a21f3f56cbd93edb72755c0c970
nel215 <otomo.yuhei@gmail.com>
2014-07-21 01:50:16 +0900

    mod some types.

--- auto_encoder.py
+++ auto_encoder.py
@@ -5,23 +5,24 @@ import random, numpy
 
 class AutoEncoder:
     def __init__(self, V, H):
-        # TODO: add bias term.
         self.V = V
         self.H = H
         # TODO: initialize weight with good defaults.
         self.weight = numpy.array([[1.0 - random.random()*2 for x in xrange(self.V)] for y in xrange(self.H)])
 
-    def train(self, samples, alpha = 0.05):
-        delta = numpy.array([[0.0 for x in xrange(self.V)] for y in xrange(self.H)]).transpose()
-        for x in samples:
+    def train(self, features, alpha = 0.05):
+        delta = numpy.array([[0.0 for x in xrange(self.V)] for y in xrange(self.H)])
+        for x in features:
+            # TODO: add bias term.
+            x = numpy.array([x]).transpose()
             y = self.encode(x)
             z = self.decode(y)
 
             sum = numpy.dot(self.weight, x-z)
-            delta += numpy.dot((x-z), y.transpose())
-            delta += numpy.dot(x, numpy.multiply(numpy.multiply(y, 1.0-y), sum).transpose())
+            delta += numpy.dot((x-z), y.transpose()).transpose()
+            delta += numpy.dot(x, numpy.multiply(numpy.multiply(y, 1.0-y), sum).transpose()).transpose()
 
-        self.weight += alpha * delta.transpose() / len(samples)
+        self.weight += alpha * delta / len(features)
 
     def encode(self, x):
         h1 = numpy.dot(self.weight, x)
@@ -31,9 +32,11 @@ class AutoEncoder:
         h2 = numpy.dot(self.weight.transpose(), y)
         return sigmoid(h2)
 
-    def error(self, samples):
+    def error(self, features):
         error = numpy.array([[0.0] for x in xrange(self.V)])
-        for x in samples:
+        for x in features:
+            # TODO: add bias term.
+            x = numpy.array([x]).transpose()
             y = self.encode(x)
             z = self.decode(y)
             error += numpy.multiply(x, numpy.log(z)) - numpy.multiply((1.0 - x), numpy.log(1.0 - z))
@@ -44,21 +47,20 @@ class AutoEncoder:
 if __name__=='__main__':
     features, labels = get_binary_dataset()
 
+    N = len(features)
     V = len(features[0])
     H = 2*V
 
     aa = AutoEncoder(V,H)
 
+    for i in xrange(1000):
+        j = int(random.random()*N)
+        aa.train(features[j:j+10])
+        if i<100 or i%1000 == 0:
+            print aa.error(features)
 
-    #for i in xrange(10000):
-    #    j = int(random.random()*len(samples))
-    #    #print samples[j:j+10]
-    #    aa.train(samples[j:j+10])
-    #    if i<100 or i%1000 == 0:
-    #        print aa.error(samples)
-
-    #for sample in samples:
-    #    print numpy.abs(sample - aa.decode(aa.encode(sample)))
+    for x in features:
+        print numpy.abs(x - aa.decode(aa.encode(x)))
 
 
 
